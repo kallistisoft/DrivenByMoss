f56c30d53f52709da40df6a7e0c6553185a0b4c8
Kallistisoft <77658107+kallistisoft@users.noreply.github.com>
2021-07-01 17:02:17 -0500

    Changed the functions of the launchpad Shuffle View rate buttons so that the left one steps through shuffle rate and the right one steps through accent rate

--- src/main/java/de/mossgrabers/controller/novation/launchpad/view/LaunchpadShuffleView.java
+++ src/main/java/de/mossgrabers/controller/novation/launchpad/view/LaunchpadShuffleView.java
@@ -54,7 +54,7 @@ public class LaunchpadShuffleView extends ShuffleView<LaunchpadControlSurface, L
         }
 
         // Shuffle rate
-        if (note == 38 || note == 39)
+        if (note == 38)
         {
             final IGroove groove = this.model.getGroove ();
             final IParameter rateParameter = groove.getParameter (GrooveParameterID.SHUFFLE_RATE);
@@ -63,6 +63,37 @@ public class LaunchpadShuffleView extends ShuffleView<LaunchpadControlSurface, L
                 final int rate = rateParameter.getValue () == 0 ? 1 : 0;
                 rateParameter.setNormalizedValue (rate);
                 this.surface.getDisplay().notify ("Groove Rate " + (rate == 1? "1/16th" : "1/8th") );
+                return;
+            }
+        }
+
+        // Shuffle accent
+        if (note == 39)
+        {
+            final IGroove groove = this.model.getGroove ();
+            final IParameter accentParameter = groove.getParameter (GrooveParameterID.ACCENT_RATE);
+            if (accentParameter != null)
+            {
+                String accentRateString = "unknown";
+
+                // circular-select next accent rate value
+                int accentRate = accentParameter.getValue();
+                switch( accentRate ) {
+                    case 0:
+                        accentRateString = "1/8th";
+                        accentRate = 64;
+                        break;
+                    case 64:
+                        accentRateString = "1/16th";
+                        accentRate = 127;
+                        break;
+                    case 127:
+                        accentRateString = "1/4";
+                        accentRate = 0;
+                        break;
+                }
+                accentParameter.setValue (accentRate);
+                this.surface.getDisplay().notify ("Groove Accent " + accentRateString );
                 return;
             }
         }
@@ -82,13 +113,33 @@ public class LaunchpadShuffleView extends ShuffleView<LaunchpadControlSurface, L
         if (enabledParameter == null || !enabledParameter.doesExist ())
             return;
 
+        // set color for groove enabled state
         this.padGrid.lightEx (0, 7, enabledParameter.getValue () == 0 ? LaunchpadColorManager.LAUNCHPAD_COLOR_GREY_HI : LaunchpadColorManager.LAUNCHPAD_COLOR_GREEN_HI);
 
+        // set color for groove shuffle rate
         final IParameter rateParameter = groove.getParameter (GrooveParameterID.SHUFFLE_RATE);
         if (rateParameter == null || !rateParameter.doesExist ())
             return;
         final boolean isEight = rateParameter.getValue () == 0;
         this.padGrid.lightEx (2, 7, isEight ? LaunchpadColorManager.LAUNCHPAD_COLOR_BLUE_HI : LaunchpadColorManager.LAUNCHPAD_COLOR_GREY_HI);
-        this.padGrid.lightEx (3, 7, !isEight ? LaunchpadColorManager.LAUNCHPAD_COLOR_BLUE_HI : LaunchpadColorManager.LAUNCHPAD_COLOR_GREY_HI);
+
+        // set color for groove accent rate
+        final IParameter accentParameter = groove.getParameter (GrooveParameterID.ACCENT_RATE);
+        if (accentParameter == null || !accentParameter.doesExist ())
+            return;
+        final int accentRate = accentParameter.getValue();
+        int accentColor = LaunchpadColorManager.LAUNCHPAD_COLOR_GREY_LO;
+        switch( accentRate ) {
+            case 0:
+                accentColor = LaunchpadColorManager.LAUNCHPAD_COLOR_RED_HI;
+                break;
+            case 64:
+                accentColor = LaunchpadColorManager.LAUNCHPAD_COLOR_BLUE_HI;
+                break;
+            case 127:
+                accentColor = LaunchpadColorManager.LAUNCHPAD_COLOR_GREY_HI;
+                break;
+        }
+        this.padGrid.lightEx (3, 7, accentColor );
     }
 }
